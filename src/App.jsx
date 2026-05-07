b05db2abc922773d10742fe2f3460c2361ef4217
kiet <mkla1252006@gmail.com>
2026-05-07 22:15:35 +0700

    dar-light-Mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import Button from '@mui/material/Button'
 import MenuIcon from '@mui/icons-material/Menu'
 import AppleIcon from '@mui/icons-material/Apple'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
-      <Typography variant="body2" color="text.secondary">
+      <ModeToggle />
+      <hr />
+      <Typography variant="p" color="text.secondary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum facere
         animi reprehenderit, ex a placeat. Id eum, repudiandae eveniet, natus
         fuga ad molestiae at accusantium nihil corrupti obcaecati assumenda
